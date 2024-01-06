47188c95a70dcb2cfe8ba70702aad7fc1eeed868
zoe becker 😹 <oiv13@txstate.edu>
2024-01-05 18:38:20 -0600

    work in progress :)

--- App.js
+++ App.js
@@ -1,12 +1,29 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { ScrollView, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
+  const Cat = (props) => {
+    <View>
+      <Text> 😺 </Text>
+      <Text> Meow! I am {props.name}! </Text>
+    </View>;
+  };
+
   return (
-    <View style={styles.container}>
-      <Text> Hello world!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView contentContainerStyle={styles.container}>
+      <View>
+        <Text> Hello! 😹</Text>
+        <StatusBar style="auto" />
+      </View>
+      <TextInput
+        style={{
+          height: 40,
+          borderColor: "gray",
+          borderWidth: 1,
+        }}
+        defaultValue="Type a cat name here!"
+      />
+    </ScrollView>
   );
 }
 
